b6c6087d4013a936f537cb2b8c30b221e542276d
JcT0913 <tjc9606ok@gmail.com>
2022-12-12 14:04:01 -0500

    imported Menu scene for Haunted House

---
+++ Haunted/Assets/BlackFadeController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.UI;
+using UnityEngine.UI;
+
+public class BlackFadeController : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Image>().enabled = true;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButton()
+    {
+        Debug.Log("Play Button Pressed");
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void ExitButton()
+    {
+        Debug.Log("Exit Button Pressed");
+        Application.Quit();
+    }
+
+    public void OnFadeComplete()
+    {
+        Debug.Log("Load Scene 1");
+        SceneManager.LoadScene(1);
+    }
+}
